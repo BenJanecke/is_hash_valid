ee85ad9468e2a4aee98105efffc8556b43cc17e5
Ben Janecke <benjanecke@gmail.com>
2015-02-03 21:34:56 +0200

    up version to 1.0

--- spec/is_hash_valid/version_spec.rb
+++ spec/is_hash_valid/version_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 require './lib/is_hash_valid/version.rb'
 
 describe "Version" do
-  it { expect(IsHashValid::VERSION).to eq("0.0.1") }
+  it { expect(IsHashValid::VERSION).to eq("1.0.0") }
 end
